f6ae8592a0dcb3aaec385a5e31fe624d57f55201
John Pancoast <johnpancoaster@gmail.com>
2016-04-15 17:24:47 -0700

    Update install

--- install.sh
+++ install.sh
@@ -3,31 +3,24 @@
 bold=`tput bold`
 normal=`tput sgr0`
 
-echo "${bold}Setting up Dot Files for this machine!${normal}"$'\n'
+echo -e "${bold}Setting up Dot Files for this machine!${normal}\n"
+echo -e "${bold}** This will overwrite files in ~/ **${normal}\n"
+
+echo "Getting submodules... "
+git submodule init
+git submodule update
 
 # Update PHP Syntax for this machine
-#php vim-php-syntax/update_syntax.php
-#echo "PHP Syntax updated based on installed packages..."
-
-# Remove any existing .vim folder
-#if [ -e ~/.vim ]; then
-#    sudo rm -r ~/.vim
-#fi
+php vim-php-syntax/update_syntax.php
+echo "PHP Syntax updated based on installed packages..."
 
 # Copy the required files to the home directory
-cp -r .bash_git .bash_profile .bashrc .gitconfig .gitignore_global .vimrc .vim .git-completion.bash .symfony-completion.bash  ~/
-echo "Dot Files copied into your home directory..."
-echo "Done."$'\n\n'
-
-# Setup .gitconfig
-#echo "${bold}Let's configure your Git configuration!${normal}"$'\n'
-#echo "What is your full name? (ex: John Doe)"
-#read name
-#echo "And what is your email address? (ex: test@gmail.com)"
-#read email
+echo -n "Copying dotfiles into home directory... "
+cp -r .bash_git .bash_profile .bashrc .gitconfig .gitignore_global .vimrc .vim .git-completion.bash .symfony-completion.bash ~/
+cp -r vim-colorschemes/colors/*.vim ~/.vim/colors/
 
 #echo "[user]"$'\n\t'"email = $email"$'\n\t'"name = $name"$'\n' >> ~/.gitconfig
-
 source ~/.bashrc
 
-echo $'\n'"** Finished!"
+echo "Finished!"
+
