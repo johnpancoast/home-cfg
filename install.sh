bd59c3c9fd34c52675b1a46c85bdd5ef6a0eac93
John Pancoast <johnpancoaster@gmail.com>
2021-03-05 01:22:13 -0800

    Add todo to install to use hardlinks for dotfiles
    This will simply allow simpler editing and simpler committing after the
    fact rather than copying files back and forth. It also means that the
    two files will share and inode and content.

--- install.sh
+++ install.sh
@@ -15,6 +15,13 @@ php vim-php-syntax/update_syntax.php
 echo "PHP Syntax updated based on installed packages..."
 
 # TODO Consider using hardlinks instead.
+# TODO DEFINITELY use hardlinkes instead for dotfiles. Dotfiles are edited
+#      frequently and then committed to this repo so it just makes sense to
+#      hard link them for ease. I cannot think of any negative effects. If
+#      anything, we get a backup of exact dotfiles since their inode and
+#      content are the same IF I wasn't so tired I'd just write the simple for
+#      loop right now, but then testing, etc. It can wait with a todo for now.
+#
 # Copy the required files to the home directory
 echo -n "Copying dotfiles into home directory... "
 cp .bash_profile .bashrc .bash_aliases .gitconfig .gitignore_global .vimrc .ideavimrc .vim .git-prompt.bash .git-completion.bash .symfony-completion.bash .screenrc ~/
